f6f5f55b543196fa1f2df14b4078669cc364f719
Emanuel Rivera A <riveraaemanuel8418@gmail.com>
2020-06-29 16:35:19 -0600

    modifiqué operaciones.js y principal.html

---
+++ operaciones.js
@@ -0,0 +1,18 @@
+
+'use strict'
+
+window.onload = function(){
+
+
+    var txt_numero = document.querySelector('#txt_numero').Value;
+    const btn_calcular = document.querySelector('#btn_calcular')
+
+    btn_calcular.addEventListener('click', function(event) {
+        alert(txt_numero);
+    });
+
+
+
+
+
+}
