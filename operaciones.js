ffec67bd89e307ea11d1c979a39c832ab8ddd84b
Emanuel Rivera A <riveraaemanuel8418@gmail.com>
2020-07-01 17:04:45 -0600

    modifique el archivo operaciones.js

--- operaciones.js
+++ operaciones.js
@@ -6,14 +6,12 @@ window.onload = function(){
     var txt_numero = null;
     const btn_calcular = document.querySelector('#btn_calcular');
 
-    const txt_p = document.getElementById('txt_p').value;
-    const txt_q = document.getElementById('txt_q').value;
+    const txt_p = document.getElementById('txt_p');
+    const txt_q = document.getElementById('txt_q');
     const txt_n = document.getElementById('txt_n');
     const txt_x = document.getElementById('txt_x');
     const btn_calcular_binomial = document.getElementById('btn_calcular_binomial');
-    var factorial_n = null;
-    var factorial_x = null;
-
+    
 
     btn_calcular.addEventListener('click', function(event) {
         event.preventDefault();
@@ -36,11 +34,9 @@ window.onload = function(){
         
         let numero_n = txt_n.value;
         let numero_x = txt_x.value;
+        let distribucion_binomial = 0;
 
-        factorial_n = calcularFactorial(numero_n);
-        factorial_x = calcularFactorial(numero_x);
-
-        let distribucion_binomial = factorial_x * numero_n - numero_x;
+        distribucion_binomial = calcularFactorial(numero_n) / (calcularFactorial(numero_x) * calcularFactorial(numero_n - numero_x));
         console.log('la distribucion binomial es: ' + distribucion_binomial);
     }
 
@@ -54,6 +50,8 @@ window.onload = function(){
         }
 
         console.log('el factorial de '+ numero + ' = ' + resultado);
+
+        return resultado;        
     }
 
     
